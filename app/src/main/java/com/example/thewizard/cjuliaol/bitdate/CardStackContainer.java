04d0302930c6d04072dc4e9b795908ecd5648d16
cjuliaol <cjuliaol@gmail.com>
2015-09-17 13:57:23 -0400

    Switching and adding New Card

--- app/src/main/java/com/example/thewizard/cjuliaol/bitdate/CardStackContainer.java
+++ app/src/main/java/com/example/thewizard/cjuliaol/bitdate/CardStackContainer.java
@@ -3,14 +3,18 @@ package com.example.thewizard.cjuliaol.bitdate;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
 
 /**
  * Created by cjuliaol on 12-Sep-15.
  */
-public class CardStackContainer extends RelativeLayout implements View.OnTouchListener{
+public class CardStackContainer extends RelativeLayout implements View.OnTouchListener {
 
     private static final String TAG = "CardStackContainerLog";
     private CardAdapter mCardAdapter;
@@ -18,19 +22,59 @@ public class CardStackContainer extends RelativeLayout implements View.OnTouchLi
     private float mLastTouchY;
     private float mPositionX;
     private float mPositionY;
+    private float mOriginX;
+    private float mOriginY;
+    private GestureDetector mGestureDetector;
+    private CardView mFrontCard;
+    private CardView mBackCard;
+    private int mNextPosition;
+
 
     public void setCardAdapter(CardAdapter cardAdapter) {
         mCardAdapter = cardAdapter;
-        if(mCardAdapter.getCount()>0) {
-            CardView cardView = mCardAdapter.getView(0,null,this);
+        if (mCardAdapter.getCount() > 0) {
+            CardView cardView = mCardAdapter.getView(0, null, this);
             cardView.setOnTouchListener(this);
+            cardView.setCardElevation(8);
+            cardView.setTranslationY(30);
+            mFrontCard = cardView;
             addView(cardView);
-
+            mNextPosition++;
         }
+
+        addBackCard();
+    }
+
+    private void addBackCard() {
+        if (mCardAdapter.getCount() > mNextPosition) {
+            CardView cardView = mCardAdapter.getView(mNextPosition, null, this);
+            mBackCard = cardView;
+            mBackCard.setCardElevation(8);
+            addView(cardView);
+            mNextPosition++;
+        }
+
+        bringChildToFront(mFrontCard);
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+
+        if (mGestureDetector.onTouchEvent(event)) {
+
+            if (mOriginX - v.getX() < 0) {
+                // To the right
+               swipeCard(true);
+            } else if (mOriginX - v.getX() > 0) {
+                // To the left
+                swipeCard(false);
+            }
+
+
+            removeView(v);
+            return true;
+        }
+
 
         int action = event.getAction();
         switch (action) {
@@ -40,10 +84,12 @@ public class CardStackContainer extends RelativeLayout implements View.OnTouchLi
 
                 mPositionX = v.getX();
                 mPositionY = v.getY();
+                mOriginX = v.getX();
+                mOriginY = v.getY();
 
                 break;
             case MotionEvent.ACTION_UP:
-
+                reset(v);
                 break;
             case MotionEvent.ACTION_MOVE:
                 float xMove = event.getX();
@@ -64,6 +110,46 @@ public class CardStackContainer extends RelativeLayout implements View.OnTouchLi
         return true;
     }
 
+    public void swipeRight() {
+        swipeCard(true);
+    }
+
+    public void swipeLeft() {
+      swipeCard(false);
+    }
+
+    public void swipeCard(boolean swipeRight) {
+        if (swipeRight == true) {
+            mFrontCard.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.slide_right));
+        } else {
+            mFrontCard.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.slide_left));
+        }
+        removeView(mFrontCard);
+
+        if (mBackCard != null) {
+            mBackCard.animate()
+                    .translationY(0)
+                    .setDuration(200);
+            mBackCard.setOnTouchListener(this);
+            mFrontCard = mBackCard;
+            mBackCard = null;
+            addBackCard();
+
+        }
+
+    }
+
+    private void reset(View v) {
+        mPositionX = mOriginX;
+        mPositionY = mOriginY;
+        v.animate()
+                .setDuration(200)
+                .setInterpolator(new AccelerateInterpolator())
+                .x(mPositionX)
+                .y(mPositionY);
+
+    }
+
     public CardStackContainer(Context context) {
         this(context, null, 0);
     }
@@ -74,7 +160,15 @@ public class CardStackContainer extends RelativeLayout implements View.OnTouchLi
 
     public CardStackContainer(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mGestureDetector = new GestureDetector(context, new FlingListener());
     }
 
+    private class FlingListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.d(TAG, "Fling happened");
+            return true;
+        }
+    }
 
 }
--- app/src/main/java/com/example/thewizard/cjuliaol/bitdate/ChoosingFragment.java
+++ app/src/main/java/com/example/thewizard/cjuliaol/bitdate/ChoosingFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,8 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class ChoosingFragment extends Fragment {
+
+    private CardStackContainer mCardStackContainer;
 
     public ChoosingFragment() {
     }
@@ -24,15 +27,40 @@ public class ChoosingFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
 
-        User user = new User();
-        user.setFirstName("Veronica");
+        User user1 = new User();
+        user1.setFirstName("Veronica");
+
+        User user2 = new User();
+        user2.setFirstName("Carla");
+
+
+        User user3 = new User();
+        user3.setFirstName("Peter");
 
         List<User> users = new ArrayList<>();
-        users.add(user);
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
 
-        CardStackContainer cardStackContainer = (CardStackContainer) view.findViewById(R.id.card_stack);
-        CardAdapter cardAdapter = new CardAdapter(getActivity(),users);
-        cardStackContainer.setCardAdapter(cardAdapter);
+        mCardStackContainer = (CardStackContainer) view.findViewById(R.id.card_stack);
+        CardAdapter cardAdapter = new CardAdapter(getActivity(), users);
+        mCardStackContainer.setCardAdapter(cardAdapter);
+
+        Button nahButton = (Button) view.findViewById(R.id.nah_button);
+        nahButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCardStackContainer.swipeLeft();
+            }
+        });
+
+        Button yeahButton = (Button) view.findViewById(R.id.yeah_button);
+        yeahButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCardStackContainer.swipeRight();
+            }
+        });
 
         return view;
     }
